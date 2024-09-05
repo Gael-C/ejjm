43ef6c4cc48da5306cc1b03866ab7b53b6d2e4e3
Gaël La Rochelle <gael_coupe17@outlook.fr>
2024-09-05 10:52:29 +0200

    Blog OK

--- resources/views/livewire/blog-page.blade.php
+++ resources/views/livewire/blog-page.blade.php
@@ -1,7 +1,7 @@
 <div class="container px-6 py-10 mx-auto">
     <div class="lg:flex lg:-mx-6">
         <!-- Section "latest" -->
-        <div class="lg:w-3/4 lg:px-6">
+        <div class="lg:w-3/4 lg:px-6 text-center">
             <a href="{{ route('article', $latest->slug) }}" class="flex flex-col items-center">
                 <img class="object-cover w-auto h-auto xl:h-[28rem] rounded-xl mx-auto" src="{{ asset('storage/'.$latest->image) }}" alt="Image de l'article du blog">
                 <div>
@@ -17,7 +17,7 @@
         <!-- Section "articles" -->
         <div class="lg:w-1/4 lg:px-6">
             @foreach ($articles as $article)
-            <div class="mt-8 lg:mt-0">
+            <div class="mt-8 lg:mt-0 text-center">
                 <a href="{{ route('article', $article->slug) }}" class="block mt-2 font-medium text-gray-700 hover:underline hover:text-gray-500 dark:text-gray-400">
                     <div>
                         <h3 class=" text-gray-800 dark:text-white capitalize">{{ $article->titre }}</h3>
